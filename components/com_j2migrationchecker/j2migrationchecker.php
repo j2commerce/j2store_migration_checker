994f8fa27f7e9c9cd19a209ace47c527e8efe1e0
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2024-11-09 16:42:37 -0500

    Copyright changes

--- components/com_j2migrationchecker/j2migrationchecker.php
+++ components/com_j2migrationchecker/j2migrationchecker.php
@@ -1,1 +1,10 @@
-Hello World
+<?php
+/**
+ * @copyright Copyright (C) 2014-2019 Weblogicx India. All rights reserved.
+ * @copyright Copyright (C) 2024 J2Commerce, Inc. All rights reserved.
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3 or later
+ * @website https://www.j2commerce.com
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
