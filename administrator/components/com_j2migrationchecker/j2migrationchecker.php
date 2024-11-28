439ef9376e12f7e35566fabc1a19e7f589b4ce30
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2024-11-28 16:38:04 -0500

    Improved app exit

--- administrator/components/com_j2migrationchecker/j2migrationchecker.php
+++ administrator/components/com_j2migrationchecker/j2migrationchecker.php
@@ -7,26 +7,30 @@
  */
 
 // No direct access to this file
+use Joomla\CMS\Factory;
+use Joomla\CMS\Router\Route;
+
 defined('_JEXEC') or die('Restricted access');
 if(!defined('F0F_INCLUDED')) {
     require_once JPATH_LIBRARIES . '/f0f/include.php';
 }
+
 if(!defined('F0F_INCLUDED')) {
-    ?>
-    <h2>Incomplete installation detected</h2>
-    <?php
+    Factory::getApplication()->enqueueMessage('The library used by J2Store was not detected. You cannot run the J2Store Migration Checker.', 'error');
+    Factory::getApplication()->redirect(Route::_('index.php?option=com_config'));
 }
+
 //if j2store does not exist, just exit
 if(!JFile::exists(JPATH_ADMINISTRATOR.'/components/com_j2store/j2store.php')) {
-    echo '<h2>Incomplete installation detected</h2>';
-    jexit(0);
+    Factory::getApplication()->enqueueMessage('J2Store was not detected. You cannot run the J2Store Migration Checker.', 'error');
+    Factory::getApplication()->redirect(Route::_('index.php?option=com_config'));
 }
 
 if(!class_exists('J2StoreStrapper')){
     require_once (JPATH_ADMINISTRATOR.'/components/com_j2store/helpers/strapper.php');
 }
+
 J2StoreStrapper::addJS();
 J2StoreStrapper::addCSS();
 
 F0FDispatcher::getTmpInstance('com_j2migrationchecker')->dispatch();
-
